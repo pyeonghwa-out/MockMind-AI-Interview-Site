ff0110c4a0322ee07c2be269e157cd7d6189a610
Raj <deeprajg077@gmail.com>
2026-06-13 10:26:57 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory.jsx'
 import Pricing from './pages/Pricing.jsx'
 import InterviewReport from './pages/InterviewReport.jsx'
 
-export const ServerUrl = "http://localhost:8000"
+export const ServerUrl = "https://mockmind-0f67.onrender.com"
 
 function App() {
   const dispatch = useDispatch()
